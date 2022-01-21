eb64696a3e88ff6e46e622c087741fe8f6ba48cd
maisoncnry12 <caxxry@gmail.com>
2022-01-21 02:04:35 +0000

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,54 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+var title = "javascriptが使えました";
+alert(title);
+
+
+// $(document).ready(function(){
+//   $(".jquery").on("click",function(){
+//     $(this).css("color","red");
+//   });
+// });
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    // スライドショーの変化("fade" or "slide")
+    transition : "slide",
+    // 変化にかかる時間（ミリ秒）
+    speed : 1000,
+    // easingの種類
+    easing : "easeOutQuart",
+    // ナビゲーションの形（"block" or "bubble")
+    navType : "block"
+    // 小要素の種類（"div" or "img"）
+    childrenElementType : "div",
+    // ナビゲーション矢印の表示
+    arrows : "true",
+    // スライドショーの自動再生
+    autoPlay : "true",
+    // 自動再生時のスライド切り替え間隔（ミリ秒）
+    autoPlayDuration : "3000",
+    // キーボードの矢印キーによるスライド送りの設定
+    keyboardDuration : "true",
+    // 一枚目のスライド表示時に戻る矢印を表示するか
+    hidePrevious : "true"
+  })
+})
+
+
